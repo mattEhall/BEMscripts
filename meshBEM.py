7312e44a11d5fbb0fe5a71cf3a4a5437fee5a947
dav-og <12831266+dav-og@users.noreply.github.com>
2020-07-14 11:24:18 -0600

    modified meshBEM.py

--- meshBEM.py
+++ meshBEM.py
@@ -1,4 +1,3 @@
-import math
 import numpy as np
 import matplotlib.pyplot as plt
 import xarray as xr
@@ -6,6 +5,7 @@ import logging
 import subprocess
 import time
 
+#from numpy import pi, sin, cos, linspace, full, concatenate
 from mpl_toolkits.mplot3d import Axes3D
 from capytaine import (FloatingBody, BEMSolver, RadiationProblem,
                        DiffractionProblem, assemble_dataset)
@@ -216,7 +216,7 @@ def plot_profile(x, z):
     plt.show()
 
 
-def create_3d_panels(xLen, nTheta):
+def axisym_3d_panels(xLen, nTheta):
     """
     define nodes in each panel for x-z points revolved around z-axis
 
@@ -244,9 +244,9 @@ def create_3d_panels(xLen, nTheta):
     return panels
 
 
-def create_3d_points(x, z, nTheta):
+def axisym_3d_points(x, z, nTheta):
     '''take x, z coordinates and revolve around z axis at 360/nTheta spacing'''
-    d2r = math.pi/180
+    d2r = np.pi/180
     theta = np.linspace(0, 360*d2r, nTheta)
     x3d = np.zeros(len(x)*nTheta)
     y3d = np.zeros(len(x)*nTheta)
@@ -255,8 +255,8 @@ def create_3d_points(x, z, nTheta):
 
     for i in range(nTheta):
         for j in range(len(x)):
-            x3d[iPoint] = x[j]*math.cos(theta[i])
-            y3d[iPoint] = x[j]*math.sin(theta[i])
+            x3d[iPoint] = x[j]*np.cos(theta[i])
+            y3d[iPoint] = x[j]*np.sin(theta[i])
             z3d[iPoint] = z[j]
             iPoint = iPoint+1
 
@@ -265,16 +265,38 @@ def create_3d_points(x, z, nTheta):
 
 class Mesh:
 
-    def __init__(self, meshName, x3d, y3d, z3d, panels):
+    def __init__(self, meshName, points, panels, cog=[0.0, 0.0, 0.0]):
         self.meshName = meshName
-        self.x3d = x3d
-        self.y3d = y3d
-        self.z3d = z3d
+        self.points = points
+        self.x3d = self.points[0,:]
+        self.y3d = self.points[1,:]
+        self.z3d = self.points[2,:]
         self.panels = panels
-        self.numPoints = len(x3d)
+        self.numPoints = len(self.points[0,:])
         self.numPanels = np.max(panels.shape)
-        self.nemohSaveName = f'{meshName}{self.numPanels}.nemoh'
-        self.wamitSaveName = f'{meshName}{self.numPanels}.gdf'
+        self.nemohSaveName = f'{meshName}.nemoh'
+        self.wamitSaveName = f'{meshName}.gdf'
+        self.cog = np.asarray(cog)
+        self.xBody = np.append(self.cog, 0.0)
+
+    def shear_x(self, xShearFactor):
+        shearMat = np.array([[1,0,0], [0,1,0], [-xShearFactor,0,1]])
+        for i in range(self.numPoints):
+            vec = self.points[:,i]
+            self.points[:,i] = vec.dot(shearMat)
+        self.cog = self.cog.dot(shearMat)
+
+    def scale_z(self, zScaleFactor):
+        self.points[2,:] *= zScaleFactor
+        self.cog[2] *= zScaleFactor
+
+    def translate_x(self, xTranslateFactor):
+        self.points[0,:] += xTranslateFactor
+        self.cog[0] += xTranslateFactor
+
+    def translate_z(self, zTranslateFactor):
+        self.points[2,:] += zTranslateFactor
+        self.cog[2] += zTranslateFactor
 
     def set_axes_equal(self, ax):
         '''Make axes of 3D plot have equal scale'''
@@ -297,12 +319,12 @@ class Mesh:
         ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
         ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
 
-    def plot_points_3d(self, x3d, y3d, z3d):
+    def plot_points_3d(self):
         '''plot 3D points'''
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
-        ax.scatter(x3d, y3d, z3d)
-        set_axes_equal(ax)
+        ax.scatter(self.x3d, self.y3d, self.z3d)
+        self.set_axes_equal(ax)
         plt.show()
 
     def save_to_nemoh_mesh(self, saveName=None):
@@ -343,7 +365,7 @@ class Mesh:
 
     def write_wamit_pot(self, potFileName, meshFileNames, nBodys, xBodys,
                         waterDepth=-1, iRad=1, iDiff=1, nPer=-402,
-                        per=[-0.02, 0.02], nBeta=1, beta=0.0):
+                        per=[-0.02, 0.02], nBetas=1, betas=[0.0]):
         '''write wamit .pot file'''
         f = open(potFileName, 'w')
         f.write(f'{potFileName}\n')
@@ -352,7 +374,9 @@ class Mesh:
         f.write(f' {nPer:<32} NPER\n')
         f.write(f' {per[0]:<8}{per[1]:<24} PER\n')
         f.write(f' {nBeta:<32} NBETA\n')
-        f.write(f' {beta:<32} BETA\n')
+        for beta in betas:
+            f.write(f' {beta:<8}')
+        f.write(f'BETA\n')
         f.write(f' {nBodys:<32} NBODY\n')
         for i, mesh in enumerate(meshFileNames):
             f.write(f' {mesh:<32}\n')
@@ -395,6 +419,90 @@ class Mesh:
         f.write(f' {0:<24d} NFIELD\n')
         f.close()
 
+    def write_wamit_config(self, ramGBMax=60.0, numCPU=10, licensePath=f'\wamitv7\license'):
+        '''write wamit config.wam file'''
+        f = open(f'config.wam', 'w')
+        f.write(f' generic configuration file:  config.wam\n')
+        f.write(f' RAMGBMAX={ramGBMax}\n')
+        f.write(f' NCPU={numCPU}\n')
+        f.write(f' USERID_PATH={licensePath} \t (directory for *.exe, *.dll, and userid.wam)\n')
+        f.write(f' LICENSE_PATH={licensePath}')
+        f.close()
+
+
+class MultiMesh():
+    def __init__(self, *Meshes, modelName):
+        self.potFileName = f'{modelName}.pot'
+        self.cfgFileName = f'{modelName}.cfg'
+        self.frcFileName = f'{modelName}.frc'
+        self.nBodys = len(Meshes)
+        self.Meshes = Meshes
+        self.modelName = modelName
+
+    def write_meshes(self, bemCode='wamit'):
+        for mesh in self.Meshes:
+            if bemCode == 'wamit':
+                mesh.save_to_gdf_mesh()
+            elif bemCode == 'nemoh':
+                mesh.save_to_nemoh_format()
+
+    def write_wamit_pot(self, waterDepth=-1, iRad=1, iDiff=1, nPer=-402,
+                        per=[-0.02, 0.02], nBetas=1, betas=[0.0]):
+        '''write wamit .pot file'''
+        f = open(self.potFileName, 'w')
+        f.write(f'{self.potFileName}\n')
+        f.write(f' {waterDepth:<32} HBOT\n')
+        f.write(f' {iRad:<8}{iDiff:<24} IRAD, IDIFF\n')
+        f.write(f' {nPer:<32} NPER\n')
+        f.write(f' {per[0]:<8}{per[1]:<24} PER\n')
+        f.write(f' {nBetas:<32} NBETA\n')
+        for beta in betas:
+            f.write(f' {beta:<7}')
+        f.write(f'BETA\n')
+        f.write(f' {self.nBodys:<32} NBODY\n')
+        for mesh in self.Meshes:
+            f.write(f' {mesh.wamitSaveName:<32}\n')
+            for i in range(4):
+                f.write(f' {mesh.xBody[i]:<7}')
+            f.write(f'XBODY(1-4)\n')
+            f.write(f' {"1 1 1 1 1 1":<32} IMODE(1-6)\n')
+        f.close()
+
+    def write_wamit_fnames(self):
+        '''write wamit fnames.wam file'''
+        f = open(f'fnames.wam', 'w')
+        f.write(f'{self.cfgFileName}\n')
+        for mesh in self.Meshes:
+            f.write(f'{mesh.wamitSaveName}\n')
+        f.write(f'{self.potFileName}\n')
+        f.write(f'{self.frcFileName}\n')
+        f.close()
+
+    def write_wamit_cfg(self, iLog=1, iPerIn=2, irr=1, iSolve=1, numHeaders=1):
+        '''write wamit .cfg file'''
+        f = open(f'{self.cfgFileName}', 'w')
+        f.write(f'! {self.modelName}\n')
+        f.write(f' ILOG={iLog:<19} (1 - panels on free surface)\n')
+        f.write(f' IPERIN={iPerIn:<17} (1 - T, 2 - w)\n')
+        f.write(f' IRR={irr:<20} (0 - not remove irr freq, 1 - remove irr freq, pannels on free surface)\n')
+        f.write(f' ISOLVE={iSolve:<17} (0 - iterative solver, 1 - direct solver)\n')
+        f.write(f' NUMHDR={numHeaders:<17} (0 - no output headers, 1 - output headers)\n')
+        f.close()
+
+    def write_wamit_frc(self):
+        '''write wamit .frc file'''
+        f = open(f'{self.frcFileName}', 'w')
+        f.write(f' {self.frcFileName}\n')
+        f.write(f' {"1 0 1 0 0 0 0 0 0":<24} IOPTN(1-9)\n')
+        for i, mesh in enumerate(self.Meshes):
+            f.write(f' {"0.0":<24} VCG({i+1})\n')
+            f.write(f' {"0.0  0.0  0.0":<24}\n')
+            f.write(f' {"0.0  0.0  0.0":<24}\n')
+            f.write(f' {"0.0  0.0  0.0":<24} XPRDCT\n')
+        f.write(f' {0:<24d} NBETAH\n')
+        f.write(f' {0:<24d} NFIELD\n')
+        f.close()
+
     def write_wamit_config(self, ramGBMax=32.0, numCPU=6, licensePath=f'\wamitv7\license'):
         '''write wamit config.wam file'''
         f = open(f'config.wam', 'w')
@@ -404,3 +512,156 @@ class Mesh:
         f.write(f' USERID_PATH={licensePath} \t (directory for *.exe, *.dll, and userid.wam)\n')
         f.write(f' LICENSE_PATH={licensePath}')
         f.close()
+
+materials = {'steel' : [7850, 3.0],
+             'coated fabric' : [1400, 9.5],
+             'sea water' : [1025, 0.0]}
+
+class DiscWithTorus(Mesh):
+
+    def __init__(self, meshName, points, panels,
+                 discRadius, discThickness,
+                 torusRadiusMinor, torusThickness,
+                 discMaterial='steel',
+                 torusInnerMaterial='sea water',
+                 torusOuterMaterial='coated fabric'):
+        super().__init__(meshName, points, panels)
+        # disc properties
+        self.discRadius = discRadius
+        self.discThickness = discThickness
+        self.discRho = materials[discMaterial][0]
+        self.discVolume = np.pi*discRadius**2 * discThickness
+        self.discMass = self.discVolume * self.discRho
+        self.discIxx = (1.0/12.0) * self.discMass * (3*self.discRadius**2 +
+                                                     self.discThickness**2)
+        self.discIyy = self.discIxx
+        self.discIzz = (1.0/2.0) * self.discMass * self.discRadius**2
+
+        # torus properties
+        self.torusRadiusMinor = torusRadiusMinor
+        self.torusThickness = torusThickness
+        self.torusRadiusMinorInner = torusRadiusMinor - torusThickness
+        self.torusRadiusMajor = torusRadiusMinor + discRadius
+        self.torusInnerRho = materials[torusInnerMaterial][0]
+        self.torusOuterRho = materials[torusOuterMaterial][0]
+        self.torusSurfaceArea = (4 * np.pi**2 * self.torusRadiusMajor *
+                                 self.torusRadiusMinor) 
+        self.torusOuterVolume = (self.torus_volume(self.torusRadiusMajor,
+                                                   self.torusRadiusMinor)
+                                 - self.torus_volume(self.torusRadiusMajor,
+                                                     self.torusRadiusMinorInner))
+        self.torusInnerVolume = self.torus_volume(self.torusRadiusMajor,
+                                                  self.torusRadiusMinorInner) 
+        self.torusInnerMass = self.torusInnerVolume * self.torusInnerRho
+        self.torusOuterMass = self.torusOuterVolume * self.torusOuterRho
+        self.torusInnerIxx = self.torus_ixx(self.torusInnerMass,
+                                            self.torusRadiusMajor,
+                                            self.torusRadiusMinorInner)
+        self.torusInnerIyy = self.torusInnerIxx
+        self.torusInnerIzz =  self.torus_izz(self.torusInnerMass,
+                                             self.torusRadiusMajor,
+                                             self.torusRadiusMinorInner)
+        self.torusOuterIxx, self.torusOuterIyy, self.torusOuterIzz = (
+            self.torus_hollow_ixxyyzz(self.torusOuterRho,
+                                      self.torusRadiusMajor,
+                                      self.torusRadiusMinor,
+                                      self.torusRadiusMinorInner))
+        self.torusMass = self.torusInnerMass + self.torusOuterMass
+        self.torusIxx = self.torusInnerIxx + self.torusOuterIxx
+        self.torusIyy = self.torusInnerIyy + self.torusOuterIyy
+        self.torusIzz = self.torusInnerIzz + self.torusOuterIzz
+
+        # combined disc & torus properties
+        self.mass = self.discMass + self.torusMass
+        self.Ixx = self.discIxx + self.torusIxx
+        self.Iyy = self.discIyy + self.torusIyy
+        self.Izz = self.discIzz + self.torusIzz
+
+        # cost estimates
+        self.discCost = self.discMass * materials[discMaterial][1]
+        self.torusInnerCost = self.torusInnerMass * materials[torusInnerMaterial][1]
+        self.torusOuterCost = self.torusOuterMass * materials[torusOuterMaterial][1]
+        self.totalCost = self.discCost + self.torusInnerCost + self.torusOuterCost
+
+    def torus_volume(self, rMajor, rMinor):
+        return 2 * np.pi**2 * rMajor * rMinor**2
+
+    def torus_ixx(self, mass, rMajor, rMinor):
+        return (1.0/8.0) * mass * (4*rMajor**2 + 5*rMinor**2)
+
+    def torus_izz(self, mass, rMajor, rMinor):
+        return (1.0/4.0) * mass * (4*rMajor**2 + 3*rMinor**2)
+
+    def torus_hollow_ixxyyzz(self, density, rMajor, rMinor, rMinorInner):
+        volumeTotal = self.torus_volume(rMajor, rMinor)
+        volumeInner = self.torus_volume(rMajor, rMinorInner)
+        massTotal = volumeTotal * density
+        massInner = volumeInner * density
+        ixxTotal = self.torus_ixx(massTotal, rMajor, rMinor)
+        ixxInner = self.torus_ixx(massInner, rMajor, rMinorInner)
+        ixxOuter = ixxTotal - ixxInner
+        iyyOuter = ixxOuter
+        izzTotal = self.torus_izz(massTotal, rMajor, rMinor)
+        izzInner = self.torus_izz(massInner, rMajor, rMinorInner)
+        izzOuter = izzTotal - izzInner
+        return ixxOuter, iyyOuter, izzOuter
+
+    def write_report(self, filename=None):
+        if filename==None:
+            filename = f'./{self.meshName}.report'
+        file = open(filename, 'w')
+        currentTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        file.write(f'Report for {self.meshName} generated @ {currentTime}\n\n')
+        file.write(f'Disc radius (m): {self.discRadius:.2f}\n')
+        file.write(f'Disc thickness (m): {self.discThickness:.2f}\n')
+        file.write(f'Torus radius (m): {self.torusRadiusMinor:.2f}\n')
+        file.write(f'Torus thickness (m): {self.torusThickness:.2f}\n')
+        file.write(f'Damper Ixx (kg m^2): {self.Ixx:.3f}\n')
+        file.write(f'Damper Iyy (kg m^2): {self.Iyy:.3f}\n')
+        file.write(f'Damper Izz (kg m^2): {self.Izz:.3f}\n')
+        file.write(f'Damper mass (kg): {self.mass:.3f}\n')
+        file.write(f'Damper cost ($): {self.totalCost:.2f}\n')
+
+d2r = np.pi/180
+r2d = 180/np.pi
+
+def disc_with_torus_xsection(discRadius, discPoints, discThickness,
+                             torusRadius, torusPoints):
+    # define points along torus outer surface - in x-z cross-section
+    xCentreTorus = discRadius + torusRadius
+    zCentreTorus = 0.0
+    # angleJoint: where torus meets disc, in degs
+    angleJoint = np.sin((discThickness/2.0)/torusRadius) * r2d
+    thetaTorus = np.linspace(-(90-angleJoint)*d2r, (270-angleJoint)*d2r, torusPoints)
+    xTorus = xCentreTorus + torusRadius*np.sin(thetaTorus)
+    zTorus = zCentreTorus + torusRadius*np.cos(thetaTorus)
+
+    # define points along disc outer surface - in x-z cross-section
+    linSpaceTop = np.linspace(270, 360, discPoints)
+    cosSpaceTop = np.cos(d2r*linSpaceTop)
+    xPlateTop = xTorus[0] * cosSpaceTop
+    zPlateTop = np.full(len(xPlateTop), zTorus[0])
+    linSpaceBottom = np.linspace(180, 270, discPoints)
+    cosSpaceBottom = np.cos(d2r*linSpaceBottom)
+    xPlateBottom = xTorus[-1] * cosSpaceBottom * -1
+    zPlateBottom = np.full(len(xPlateBottom), zTorus[-1])
+
+    # concatenate, remove duplicate points
+    xPts = np.concatenate((xPlateTop[:-1], xTorus, xPlateBottom[1:]))
+    zPts = np.concatenate((zPlateTop[:-1], zTorus, zPlateBottom[1:]))
+
+    return xPts, zPts
+
+def hemisphere_xsection(hemisphereRadius, hemispherePoints):
+    linSpacing = np.linspace(0, 90, hemispherePoints)
+    cosineSpacing = np.cos(d2r*linSpacing) * (90*d2r)
+    arcXPts = hemisphereRadius*np.sin(cosineSpacing)
+    arcZPts = -hemisphereRadius*np.cos(cosineSpacing)
+    arcZPts[0] = 0.0 # enforce = 0.0 @ waterline
+    # for closed hemisphere (@ waterline):
+    topXPts = np.cos(d2r*np.linspace(270,360,int(hemispherePoints/2)))*arcXPts[0]
+    topZPts = np.full(len(topXPts), 0.0) # enforce = 0.0 (waterline)
+    xPts = np.append(topXPts[:-1], arcXPts)
+    zPts = np.append(topZPts[:-1], arcZPts)
+
+    return xPts, zPts
